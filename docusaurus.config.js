bda28ebe7a5b7e75fdfbb735ad9f5b3a093b13ec
zhoudeyou <zhoudeyou@corp.netease.com>
2021-07-18 21:26:33 +0800

    :book: feat: initial project

---
+++ docusaurus.config.js
@@ -0,0 +1,105 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: '前端周同学\'s Blog',
+  tagline: '📖 公众号: 前端周同学',
+  url: 'https://github.com/luffyZh',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'luffyZh', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-luffyzh-website', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: '前端周同学',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: '文档',
+        },
+        {
+          to: '/blog',
+          label: '博客',
+          position: 'left'
+        },
+        {
+          href: 'https://github.com/luffyZh/docusaurus-luffyzh-website',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: '文档',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Blog',
+          items: [
+            {
+              label: '博客',
+              to: '/blog',
+            },
+          ],
+        },
+        {
+          title: '更多',
+          items: [
+            {
+              label: '掘金',
+              href: 'https://juejin.cn/user/96412752681079'
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/luffzh/docusaurus-luffyzh-website',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} 前端周同学～`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
